b74cde299278ca4688ff19578694b9e901276849
Craffic <392301161@qq.com>
2022-02-22 21:27:24 +0800

    1. 搭建定时器任务模块

---
+++ job-server/src/main/java/com/craffic/convey/jobserver/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.craffic.convey.jobserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello ConveyOA job server!";
+    }
+
+}
