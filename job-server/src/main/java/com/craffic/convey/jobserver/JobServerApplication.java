bd947e8303831528a998c1f36d4c6cbad50016ee
Craffic <392301161@qq.com>
2022-02-23 23:37:09 +0800

    1. Scheduler定时器

--- job-server/src/main/java/com/craffic/convey/jobserver/JobServerApplication.java
+++ job-server/src/main/java/com/craffic/convey/jobserver/JobServerApplication.java
@@ -3,9 +3,11 @@ package com.craffic.convey.jobserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(value = "com.craffic.convey.jobserver.dao")
+@EnableScheduling
 public class JobServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobServerApplication.class, args);
