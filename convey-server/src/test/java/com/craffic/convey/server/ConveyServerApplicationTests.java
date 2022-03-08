1b51ed8d069d59a4a9f52c81020db20069c386b1
Craffic <392301161@qq.com>
2022-03-08 22:17:17 +0800

    新增Convey-Server模块

---
+++ convey-server/src/test/java/com/craffic/convey/server/ConveyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craffic.convey.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
